0679e69ab38ba4a190ef29447c7a19fd2e8ef712
wluyima <wyclif@openmrs.org>
2025-12-01 09:30:37 -0600

    MG-24: Updated the addon name and description in the manifest file

--- odoo/__manifest__.py
+++ odoo/__manifest__.py
@@ -1,12 +1,12 @@
 {
-    "name": "Ozone User Roles",
+    "name": "Ozone Auth Roles",
     "version": "17.0.0.0.0",
     "license": "AGPL-3",
     "author": (
         "Mekom Solutions"
     ),
     "website": "https://github.com/ozone-his/ozone-auth-roles/odoo",
-    "summary": "Assigns a user roles from the authentication provider upon login",
+    "summary": "Assigns the roles defined in the authentication provider to a user upon login",
     "depends": ["auth_oidc"],
     "data": [],
     "demo": [],
