d3786a6f7bd31206292e766f3bb0dd14b8346ca0
rbuisson <romain@mekomsolutions.com>
2025-12-08 22:38:10 +0100

    OZ-1027: Rework the packaging to move away from monorepo

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,12 @@
 {
-    "name": "Ozone Auth Roles",
-    "version": "17.0.0.0.0",
+    "name": "${project.name}",
+    'version': '${odooVersion}.${addonVersion}',
     "license": "AGPL-3",
     "author": (
         "Mekom Solutions"
     ),
     "website": "https://github.com/ozone-his/ozone-auth-roles/odoo",
-    "summary": "Assigns the roles defined in the authentication provider to a user upon login",
+    "summary": "Assigns the roles defined in the authentication provider to a user as groups upon login",
     "depends": ["auth_oidc"],
     "data": [],
     "demo": [],
