ba8c7763e9d030636d811d44e30c0a64c130d9d8
wluyima <wyclif@openmrs.org>
2026-02-12 20:39:54 -0600

    MG-41: Merged user update logic into a single call

--- models/res_users.py
+++ models/res_users.py
@@ -18,22 +18,7 @@ class CustomUser(models.Model):
     def assign_roles(self, credentials, params):
         user = self.search([("login", "=", credentials[1]), ('oauth_access_token', '=', params['access_token'])])
         claims = jwt.get_unverified_claims(params['access_token'])
-        odoo_access = claims['resource_access'].get('odoo')
-        if odoo_access and odoo_access.get('roles'):
-            odoo_roles = odoo_access.get('roles')
-            group_ids = []
-            for r in odoo_roles:
-                group_id = self.env["res.groups"].search([("full_name", "=", r)])
-                if group_id.id:
-                    group_ids.append(group_id.id)
-                else:
-                    _logger.warning('No group found with full name %s', r)
-            user.write({'groups_id': [(6, 0, group_ids)]})
-        else:
-            user.write({'groups_id': [(5, 0, 0)]})
-
         org_names = claims['organizations']
-
         # Odoo requires the default company to always be in the allowed companies
         company_ids = [user.company_id.id]
         if org_names:
@@ -44,4 +29,16 @@ class CustomUser(models.Model):
                 else:
                     _logger.warning('No company found with name %s', n)
 
-        user.write({'company_ids': [(6, 0, company_ids)]})
+        odoo_access = claims['resource_access'].get('odoo')
+        if odoo_access and odoo_access.get('roles'):
+            odoo_roles = odoo_access.get('roles')
+            group_ids = []
+            for r in odoo_roles:
+                group_id = self.env["res.groups"].search([("full_name", "=", r)])
+                if group_id.id:
+                    group_ids.append(group_id.id)
+                else:
+                    _logger.warning('No group found with full name %s', r)
+            user.write({'groups_id': [(6, 0, group_ids)], 'company_ids': [(6, 0, company_ids)]})
+        else:
+            user.write({'groups_id': [(5, 0, 0)], 'company_ids': [(6, 0, company_ids)]})
